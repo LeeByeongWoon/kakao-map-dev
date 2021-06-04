e3e8ec9cd254ec3ce423889c2eb7d0688a069ea7
gangbin park <gangbin.park@wizontech.com>
2021-06-04 11:41:09 +0900

    두번째 푸시

---
+++ keti-producer-weather/src/test/java/com/keti/kafka/producer/weather/KetiProducerWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keti.kafka.producer.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KetiProducerWeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
